f18597fc2c28df021d9b802293dff196e4027a7b
François <mockersf@gmail.com>
2022-09-18 03:09:40 +0200

    switch to polyanya

---
+++ examples/polyanya.rs
@@ -0,0 +1,265 @@
+use bevy::{math::Vec3Swizzles, prelude::*, sprite::MaterialMesh2dBundle};
+use bevy_pathmesh::{PathMesh, PathmeshPlugin};
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PathmeshPlugin)
+        .add_startup_system(setup)
+        .add_system(on_mesh_change)
+        .add_system(mesh_change)
+        .add_system(on_click)
+        .add_system(compute_paths)
+        .add_system(move_navigator)
+        .run();
+}
+
+struct Meshes {
+    simple: Handle<PathMesh>,
+    arena: Handle<PathMesh>,
+    aurora: Handle<PathMesh>,
+}
+
+enum CurrentMesh {
+    Simple,
+    Arena,
+    Aurora,
+}
+
+struct MeshDetails {
+    mesh: CurrentMesh,
+    size: Vec2,
+}
+
+const SIMPLE: MeshDetails = MeshDetails {
+    mesh: CurrentMesh::Simple,
+    size: Vec2::new(13.0, 8.0),
+};
+
+const ARENA: MeshDetails = MeshDetails {
+    mesh: CurrentMesh::Arena,
+    size: Vec2::new(49.0, 49.0),
+};
+
+const AURORA: MeshDetails = MeshDetails {
+    mesh: CurrentMesh::Aurora,
+    size: Vec2::new(1024.0, 768.0),
+};
+
+fn setup(
+    mut commands: Commands,
+    mut pathmeshes: ResMut<Assets<PathMesh>>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.insert_resource(Meshes {
+        simple: pathmeshes.add(PathMesh::from_polyanya_mesh(polyanya::Mesh::new(
+            vec![
+                polyanya::Vertex::new(Vec2::new(0., 6.), vec![0, -1]),
+                polyanya::Vertex::new(Vec2::new(2., 5.), vec![0, -1, 2]),
+                polyanya::Vertex::new(Vec2::new(5., 7.), vec![0, 2, -1]),
+                polyanya::Vertex::new(Vec2::new(5., 8.), vec![0, -1]),
+                polyanya::Vertex::new(Vec2::new(0., 8.), vec![0, -1]),
+                polyanya::Vertex::new(Vec2::new(1., 4.), vec![1, -1]),
+                polyanya::Vertex::new(Vec2::new(2., 1.), vec![1, -1]),
+                polyanya::Vertex::new(Vec2::new(4., 1.), vec![1, -1]),
+                polyanya::Vertex::new(Vec2::new(4., 2.), vec![1, -1, 2]),
+                polyanya::Vertex::new(Vec2::new(2., 4.), vec![1, 2, -1]),
+                polyanya::Vertex::new(Vec2::new(7., 4.), vec![2, -1, 4]),
+                polyanya::Vertex::new(Vec2::new(10., 7.), vec![2, 4, 6, -1, 3]),
+                polyanya::Vertex::new(Vec2::new(7., 7.), vec![2, 3, -1]),
+                polyanya::Vertex::new(Vec2::new(11., 8.), vec![3, -1]),
+                polyanya::Vertex::new(Vec2::new(7., 8.), vec![3, -1]),
+                polyanya::Vertex::new(Vec2::new(7., 0.), vec![5, 4, -1]),
+                polyanya::Vertex::new(Vec2::new(11., 3.), vec![4, 5, -1]),
+                polyanya::Vertex::new(Vec2::new(11., 5.), vec![4, -1, 6]),
+                polyanya::Vertex::new(Vec2::new(12., 0.), vec![5, -1]),
+                polyanya::Vertex::new(Vec2::new(12., 3.), vec![5, -1]),
+                polyanya::Vertex::new(Vec2::new(13., 5.), vec![6, -1]),
+                polyanya::Vertex::new(Vec2::new(13., 7.), vec![6, -1]),
+                polyanya::Vertex::new(Vec2::new(1., 3.), vec![1, -1]),
+            ],
+            vec![
+                polyanya::Polygon::new(vec![0, 1, 2, 3, 4], true),
+                polyanya::Polygon::new(vec![5, 22, 6, 7, 8, 9], true),
+                polyanya::Polygon::new(vec![1, 9, 8, 10, 11, 12, 2], false),
+                polyanya::Polygon::new(vec![12, 11, 13, 14], true),
+                polyanya::Polygon::new(vec![10, 15, 16, 17, 11], false),
+                polyanya::Polygon::new(vec![15, 18, 19, 16], true),
+                polyanya::Polygon::new(vec![11, 17, 20, 21], true),
+            ],
+        ))),
+        arena: asset_server.load("arena-merged.polyanya.mesh"),
+        aurora: asset_server.load("aurora-merged.polyanya.mesh"),
+    });
+    commands.insert_resource(SIMPLE);
+}
+
+fn on_mesh_change(
+    mesh: Res<MeshDetails>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    pathmeshes: Res<Assets<PathMesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    path_meshes: Res<Meshes>,
+    mut current_mesh_entity: Local<Option<Entity>>,
+    windows: Res<Windows>,
+    navigator: Query<Entity, With<Navigator>>,
+) {
+    if mesh.is_changed() {
+        let handle = match mesh.mesh {
+            CurrentMesh::Simple => &path_meshes.simple,
+            CurrentMesh::Arena => &path_meshes.arena,
+            CurrentMesh::Aurora => &path_meshes.aurora,
+        };
+        let pathmesh = pathmeshes.get(handle).unwrap();
+        if let Some(entity) = *current_mesh_entity {
+            commands.entity(entity).despawn();
+        }
+        if let Ok(entity) = navigator.get_single() {
+            commands.entity(entity).despawn();
+        }
+        let window = windows.primary();
+        let factor = (window.width() / mesh.size.x).min(window.height() / mesh.size.y);
+        *current_mesh_entity = Some(
+            commands
+                .spawn_bundle(MaterialMesh2dBundle {
+                    mesh: meshes.add(pathmesh.to_mesh()).into(),
+                    transform: Transform::from_translation(Vec3::new(
+                        -mesh.size.x / 2.0 * factor,
+                        -mesh.size.y / 2.0 * factor,
+                        0.0,
+                    ))
+                    .with_scale(Vec3::splat(factor)),
+                    material: materials.add(ColorMaterial::from(Color::BLUE)),
+                    ..default()
+                })
+                .id(),
+        );
+    }
+}
+
+fn mesh_change(mut mesh: ResMut<MeshDetails>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        match mesh.mesh {
+            CurrentMesh::Simple => *mesh = ARENA,
+            CurrentMesh::Arena => *mesh = AURORA,
+            CurrentMesh::Aurora => *mesh = SIMPLE,
+        }
+    }
+}
+
+#[derive(Component)]
+struct Navigator {
+    speed: f32,
+}
+
+#[derive(Component)]
+struct Target {
+    target: Vec2,
+    pathmesh: Handle<PathMesh>,
+}
+
+#[derive(Component)]
+struct Path {
+    path: Vec<Vec2>,
+}
+
+fn on_click(
+    mouse_button_input: Res<Input<MouseButton>>,
+    windows: Res<Windows>,
+    mesh: Res<MeshDetails>,
+    meshes: Res<Meshes>,
+    mut commands: Commands,
+    query: Query<Entity, With<Navigator>>,
+) {
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        if let Some(position) = windows.primary().cursor_position() {
+            let screen = Vec2::new(windows.primary().width(), windows.primary().height());
+            let factor = (screen.x / mesh.size.x).min(screen.y / mesh.size.y);
+
+            let in_mesh = (position - screen / 2.0) / factor + mesh.size / 2.0;
+            if (0.0..mesh.size.x).contains(&in_mesh.x) && (0.0..mesh.size.y).contains(&in_mesh.y) {
+                if let Ok(navigator) = query.get_single() {
+                    info!("going to {}", in_mesh);
+                    commands.entity(navigator).insert(Target {
+                        target: in_mesh,
+                        pathmesh: match mesh.mesh {
+                            CurrentMesh::Simple => meshes.simple.clone_weak(),
+                            CurrentMesh::Arena => meshes.arena.clone_weak(),
+                            CurrentMesh::Aurora => meshes.aurora.clone_weak(),
+                        },
+                    });
+                } else {
+                    info!("spawning at {}", in_mesh);
+                    commands
+                        .spawn_bundle(SpriteBundle {
+                            sprite: Sprite {
+                                color: Color::RED,
+                                custom_size: Some(Vec2::ONE),
+                                ..default()
+                            },
+                            transform: Transform::from_translation(
+                                (position - screen / 2.0).extend(1.0),
+                            )
+                            .with_scale(Vec3::splat(5.0)),
+                            ..default()
+                        })
+                        .insert(Navigator { speed: 50.0 });
+                }
+            } else {
+                info!("clicked outside of mesh");
+            }
+        }
+    }
+}
+
+fn compute_paths(
+    mut commands: Commands,
+    with_target: Query<(Entity, &Target, &Transform), Changed<Target>>,
+    meshes: Res<Assets<PathMesh>>,
+    mesh: Res<MeshDetails>,
+    windows: Res<Windows>,
+) {
+    for (entity, target, transform) in &with_target {
+        let window = windows.primary();
+        let factor = (window.width() / mesh.size.x).min(window.height() / mesh.size.y);
+
+        let in_mesh = transform.translation.truncate() / factor + mesh.size / 2.0;
+        let mesh = meshes.get(&target.pathmesh).unwrap();
+
+        if let Some(path) = mesh.path(in_mesh, target.target) {
+            commands.entity(entity).insert(Path { path: path.path });
+        } else {
+            info!("no path found");
+        }
+    }
+}
+
+fn move_navigator(
+    mut query: Query<(Entity, &mut Transform, &mut Path, &Navigator)>,
+    mesh: Res<MeshDetails>,
+    windows: Res<Windows>,
+    time: Res<Time>,
+    mut commands: Commands,
+) {
+    let window = windows.primary();
+    let factor = (window.width() / mesh.size.x).min(window.height() / mesh.size.y);
+    for (entity, mut transform, mut path, navigator) in &mut query {
+        let next = (path.path[0] - mesh.size / 2.0) * factor;
+        let toward = next - transform.translation.xy();
+        // TODO: compare this in mesh dimensions, not in display dimensions
+        if toward.length() < 1.0 {
+            path.path.remove(0);
+            if path.path.is_empty() {
+                debug!("reached target");
+                commands.entity(entity).remove::<Path>();
+            } else {
+                debug!("reached next step");
+            }
+        }
+        transform.translation +=
+            (toward.normalize() * time.delta_seconds() * navigator.speed).extend(0.0);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,247 +1,93 @@
-#![allow(clippy::needless_collect)]
-
 use bevy::{
+    asset::{AssetLoader, LoadContext, LoadedAsset},
     prelude::*,
-    render::{
-        mesh::{Indices, VertexAttributeValues},
-        render_resource::PrimitiveTopology,
-    },
-    utils::{HashMap, HashSet},
+    reflect::TypeUuid,
+    render::{mesh::Indices, render_resource::PrimitiveTopology},
+    utils::BoxedFuture,
 };
-use petgraph::{algo::connected_components, graphmap::UnGraphMap};
 
-struct Triangle {
-    a: (Vec3, usize),
-    b: (Vec3, usize),
-    c: (Vec3, usize),
+pub struct PathmeshPlugin;
+
+impl Plugin for PathmeshPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_asset::<PathMesh>()
+            .init_asset_loader::<PathMeshPolyanyaLoader>();
+    }
 }
 
-#[derive(Component, Default)]
-pub struct NavMesh {
-    triangles: Vec<Triangle>,
-    graph: UnGraphMap<usize, f32>,
-    vertices: Vec<Vec3>,
+#[derive(Debug, TypeUuid)]
+#[uuid = "807C7A31-EA06-4A3B-821B-6E91ADB95734"]
+pub struct PathMesh {
+    mesh: polyanya::Mesh,
 }
-impl NavMesh {
-    pub fn from_mesh(mesh: &Mesh) -> NavMesh {
-        info!("creating navmesh");
-        fn mesh_to_option(mesh: &Mesh) -> Option<NavMesh> {
-            let indices = match mesh.primitive_topology() {
-                PrimitiveTopology::TriangleList => mesh.indices()?,
-                PrimitiveTopology::TriangleStrip => mesh.indices()?,
-                _ => return None,
-            };
 
-            let indices: Vec<usize> = match indices {
-                Indices::U16(indices) => indices.iter().map(|v| *v as usize).collect(),
-                Indices::U32(indices) => indices.iter().map(|v| *v as usize).collect(),
-            };
+impl PathMesh {
+    pub fn from_polyanya_mesh(mesh: polyanya::Mesh) -> PathMesh {
+        PathMesh { mesh }
+    }
 
-            let grouped_indices = indices
+    pub fn path(&self, from: Vec2, to: Vec2) -> Option<polyanya::Path> {
+        self.mesh.path(from, to)
+    }
+
+    pub fn to_mesh(&self) -> Mesh {
+        let mut new_mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        new_mesh.insert_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            self.mesh
+                .vertices
                 .iter()
-                .fold((vec![], vec![]), |(mut triangles, mut buffer), i| {
-                    buffer.push(*i);
-                    if buffer.len() == 3 {
-                        triangles.push(buffer.clone());
-                        buffer = vec![];
-                        if mesh.primitive_topology() == PrimitiveTopology::TriangleStrip {
-                            buffer.push(*i);
-                        }
-                    }
-                    (triangles, buffer)
+                .map(|v| [v.coords.x, v.coords.y, 0.0])
+                .collect::<Vec<[f32; 3]>>(),
+        );
+        new_mesh.set_indices(Some(Indices::U32(
+            self.mesh
+                .polygons
+                .iter()
+                .flat_map(|p| {
+                    (2..p.vertices.len())
+                        .flat_map(|i| [p.vertices[0], p.vertices[i - 1], p.vertices[i]])
                 })
-                .0;
-
-            if let VertexAttributeValues::Float32x3(positions) =
-                mesh.attribute(Mesh::ATTRIBUTE_POSITION)?
-            {
-                let mut graph = UnGraphMap::with_capacity(0, 0);
-
-                let vertices: Vec<Vec3> = positions.iter().map(|p| Vec3::from_slice(p)).collect();
-
-                let mut graph_connections: HashMap<IVec3, HashSet<usize>> = HashMap::default();
-
-                let triangles = grouped_indices
-                    .iter()
-                    .map(|indices| {
-                        let a = vertices[indices[0]];
-                        let b = vertices[indices[1]];
-                        let c = vertices[indices[2]];
-                        graph_connections
-                            .entry(IVec3::new(
-                                (a.x * 1000.0) as i32,
-                                (a.y * 1000.0) as i32,
-                                (a.z * 1000.0) as i32,
-                            ))
-                            .or_insert_with(HashSet::default)
-                            .insert(indices[0]);
-                        graph_connections
-                            .entry(IVec3::new(
-                                (b.x * 1000.0) as i32,
-                                (b.y * 1000.0) as i32,
-                                (b.z * 1000.0) as i32,
-                            ))
-                            .or_insert_with(HashSet::default)
-                            .insert(indices[1]);
-                        graph_connections
-                            .entry(IVec3::new(
-                                (c.x * 1000.0) as i32,
-                                (c.y * 1000.0) as i32,
-                                (c.z * 1000.0) as i32,
-                            ))
-                            .or_insert_with(HashSet::default)
-                            .insert(indices[2]);
-
-                        graph.add_edge(indices[0], indices[1], a.distance(b));
-                        graph.add_edge(indices[0], indices[2], a.distance(c));
-                        graph.add_edge(indices[1], indices[2], b.distance(c));
-                        Triangle {
-                            a: (a, indices[0]),
-                            b: (b, indices[1]),
-                            c: (c, indices[2]),
-                        }
-                    })
-                    .collect();
-                if connected_components(&graph) > 1 {
-                    for vertices in graph_connections.values() {
-                        if vertices.len() > 1 {
-                            vertices.iter().fold(None, |last, current| {
-                                if let Some(last) = last {
-                                    let edges = graph
-                                        .edges(*current)
-                                        .map(|(a, b, w)| (a, b, *w))
-                                        .collect::<Vec<_>>();
-                                    for (from, to, weight) in edges.into_iter() {
-                                        if from == *current {
-                                            graph.add_edge(last, to, weight);
-                                        } else {
-                                            graph.add_edge(last, from, weight);
-                                        }
-                                    }
-                                }
-                                Some(*current)
-                            });
-                        }
-                    }
-                }
-                return Some(NavMesh {
-                    triangles,
-                    graph,
-                    vertices,
-                });
-            }
-            None
-        }
-        mesh_to_option(mesh).unwrap_or_default()
+                .map(|v| v as u32)
+                .collect(),
+        )));
+        new_mesh.insert_attribute(
+            Mesh::ATTRIBUTE_NORMAL,
+            (0..self.mesh.vertices.len())
+                .into_iter()
+                .map(|_| [0.0, 0.0, 1.0])
+                .collect::<Vec<[f32; 3]>>(),
+        );
+        new_mesh.insert_attribute(
+            Mesh::ATTRIBUTE_UV_0,
+            self.mesh
+                .vertices
+                .iter()
+                .map(|v| [v.coords.x, v.coords.y])
+                .collect::<Vec<[f32; 2]>>(),
+        );
+        new_mesh
     }
+}
 
-    pub fn point_in_mesh(&self, point: Vec3) -> bool {
-        self.point_to_triangle(point).is_some()
-    }
+#[derive(Default)]
+pub struct PathMeshPolyanyaLoader;
 
-    fn point_to_triangle(&self, point: Vec3) -> Option<&Triangle> {
-        self.triangles.iter().find(|triangle| {
-            point_in_triangle(&point, (&triangle.a.0, &triangle.b.0, &triangle.c.0))
+impl AssetLoader for PathMeshPolyanyaLoader {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut LoadContext,
+    ) -> BoxedFuture<'a, Result<(), bevy::asset::Error>> {
+        Box::pin(async move {
+            load_context.set_default_asset(LoadedAsset::new(PathMesh {
+                mesh: polyanya::Mesh::from_bytes(bytes),
+            }));
+            Ok(())
         })
     }
 
-    pub fn path_from_to(&self, from: Vec3, to: Vec3) -> Vec<Vec3> {
-        let mut path = self.path_option(from, to).unwrap_or_default();
-        if path.len() > 1 {
-            self.smoothen_path(from, &mut path);
-            path.push(to);
-        }
-        path
-    }
-
-    fn path_option(&self, from: Vec3, to: Vec3) -> Option<Vec<Vec3>> {
-        info!("searching for path");
-        let start = self.point_to_triangle(from)?;
-        let end = self.point_to_triangle(to)?;
-        let mut graph = self.graph.clone();
-        graph.add_edge(usize::MAX, start.a.1, from.distance(start.a.0));
-        graph.add_edge(usize::MAX, start.b.1, from.distance(start.b.0));
-        graph.add_edge(usize::MAX, start.c.1, from.distance(start.c.0));
-        graph.add_edge(usize::MAX - 1, end.a.1, from.distance(end.a.0));
-        graph.add_edge(usize::MAX - 1, end.b.1, from.distance(end.b.0));
-        graph.add_edge(usize::MAX - 1, end.c.1, from.distance(end.c.0));
-        let path = petgraph::algo::astar(
-            &graph,
-            usize::MAX,
-            |n| n == usize::MAX - 1,
-            |e| *e.2,
-            |n| {
-                if n == usize::MAX - 1 {
-                    0.0
-                } else if n == usize::MAX {
-                    from.distance(to)
-                } else {
-                    self.vertices[n].distance(to) * 0.8
-                }
-            },
-        )?
-        .1;
-        let mut path: Vec<Vec3> = path
-            .iter()
-            .filter(|n| **n != usize::MAX && **n != usize::MAX - 1)
-            .map(|n| self.vertices[*n])
-            .collect();
-        path.push(to);
-        Some(path)
-    }
-
-    fn smoothen_path(&self, from: Vec3, path: &mut Vec<Vec3>) {
-        let mut new_path = vec![];
-        let mut current = from;
-        let mut last = from;
-        let mut diff = 0;
-        for point in path.iter() {
-            diff += 1;
-            let delta = *point - current;
-            let mut is_in = true;
-            let mut last_triangle = None;
-            // Check that line is in mesh
-            let factor = 100;
-            for i in 1..(diff * factor) {
-                let to_check = current + delta * (i as f32 / (diff * factor) as f32);
-                if last_triangle
-                    .map(|triangle| point_in_triangle(&to_check, triangle))
-                    .unwrap_or(false)
-                {
-                    continue;
-                }
-                if let Some(triangle) = self.point_to_triangle(to_check) {
-                    last_triangle = Some((&triangle.a.0, &triangle.b.0, &triangle.c.0));
-                } else {
-                    is_in = false;
-                    break;
-                }
-            }
-            if !is_in {
-                new_path.push(last);
-                current = last;
-                diff = 0;
-            } else {
-            }
-            last = *point;
-        }
-
-        *path = new_path;
-    }
-}
-
-fn point_in_triangle(point: &Vec3, (va, vb, vc): (&Vec3, &Vec3, &Vec3)) -> bool {
-    let pa = *va - *point;
-    let pb = *vb - *point;
-    let pc = *vc - *point;
-
-    let u = pb.cross(pc);
-    let v = pc.cross(pa);
-    let w = pa.cross(pb);
-
-    if u.dot(v) < 0.0 {
-        false
-    } else {
-        u.dot(w) > 0.0
+    fn extensions(&self) -> &[&str] {
+        &["polyanya.mesh"]
     }
 }
