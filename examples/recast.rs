316312f0f6a17a78ba1446d5ba41669b4f14b583
François Mockers <francois.mockers@vleue.com>
2025-07-30 00:33:11 +0200

    full mesh, and can enable/disable layers

--- examples/recast.rs
+++ examples/recast.rs
@@ -9,9 +9,10 @@ use vleue_navigator::{
 mod camera_controller;
 
 fn main() {
-    // let navmesh = polyanya::RecastPolyMesh::from_file("assets/recast/poly_mesh.json").into();
-    let detailed_navmesh =
-        polyanya::RecastPolyMeshDetail::from_file("assets/recast/detail_mesh.json").into();
+    let rasterised_mesh = polyanya::RecastPolyMesh::from_file("assets/recast/poly_mesh.json");
+    let detailed_mesh = polyanya::RecastPolyMeshDetail::from_file("assets/recast/detail_mesh.json");
+    let full_navmesh: polyanya::Mesh =
+        polyanya::RecastFullMesh::new(rasterised_mesh, detailed_mesh).into();
 
     App::new()
         .insert_resource(ClearColor(Color::srgb(0., 0., 0.01)))
@@ -22,14 +23,18 @@ fn main() {
             NavmeshUpdaterPlugin::<PrimitiveObstacle>::default(),
         ))
         .add_systems(Startup, setup)
-        .add_systems(Update, (draw_navmesh, draw_path))
-        .insert_resource(RecastNavmesh(detailed_navmesh))
+        .add_systems(
+            Update,
+            (draw_navmesh, draw_path, layers_info, switch_layers),
+        )
+        .insert_resource(Layers(vec![true; full_navmesh.layers.len()]))
+        .insert_resource(RecastNavmesh(full_navmesh))
         .insert_gizmo_config::<NavMeshGizmos>(
             NavMeshGizmos,
             GizmoConfig {
                 line: GizmoLineConfig {
                     style: GizmoLineStyle::Dashed {
-                        gap_scale: 6.0,
+                        gap_scale: 1.0,
                         line_scale: 4.0,
                     },
                     ..default()
@@ -51,6 +56,14 @@ fn main() {
         .run();
 }
 
+const LAYER_COLORS: [Srgba; 5] = [
+    palettes::tailwind::BLUE_600,
+    palettes::tailwind::RED_600,
+    palettes::tailwind::GREEN_600,
+    palettes::tailwind::YELLOW_600,
+    palettes::tailwind::FUCHSIA_600,
+];
+
 #[derive(Default, Reflect, GizmoConfigGroup)]
 struct NavMeshGizmos;
 #[derive(Default, Reflect, GizmoConfigGroup)]
@@ -58,6 +71,9 @@ struct PathGizmos;
 
 #[derive(Resource)]
 struct RecastNavmesh(polyanya::Mesh);
+
+#[derive(Resource)]
+struct Layers(Vec<bool>);
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
@@ -82,20 +98,24 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 fn draw_navmesh(mut navmesh_gizmos: Gizmos<NavMeshGizmos>, recast: Res<RecastNavmesh>) {
     let mesh = &recast.0;
+    let colors: Vec<_> = LAYER_COLORS
+        .iter()
+        .map(|color| color.clone().into())
+        .collect();
     display_mesh_gizmo(
         mesh,
         &Transform::from_rotation(Quat::from_rotation_x(FRAC_PI_2)).into(),
-        &[
-            palettes::tailwind::BLUE_400.into(),
-            palettes::tailwind::CYAN_400.into(),
-            palettes::tailwind::GREEN_400.into(),
-            palettes::tailwind::YELLOW_400.into(),
-        ],
+        colors.as_slice(),
         &mut navmesh_gizmos,
     );
 }
 
-fn draw_path(mut path_gizmos: Gizmos<PathGizmos>, mut gizmos: Gizmos, recast: Res<RecastNavmesh>) {
+fn draw_path(
+    mut path_gizmos: Gizmos<PathGizmos>,
+    mut gizmos: Gizmos,
+    recast: Res<RecastNavmesh>,
+    layers: Res<Layers>,
+) {
     let start = vec3(46.998413, 9.998184, 1.717747);
     let end = vec3(20.703018, 18.651773, -80.770203);
 
@@ -103,20 +123,32 @@ fn draw_path(mut path_gizmos: Gizmos<PathGizmos>, mut gizmos: Gizmos, recast: Re
     gizmos.sphere(end, 0.5, palettes::tailwind::YELLOW_400);
 
     let mesh = &recast.0;
-    let Some(path) = mesh.path(start.xz(), end.xz()) else {
+    // let Some(path) = mesh.path(start.xz(), end.xz()) else {
+    //     return;
+    // };
+    let Some(path) = mesh.path_on_layers(
+        start.xz(),
+        end.xz(),
+        layers
+            .0
+            .iter()
+            .enumerate()
+            .filter_map(|(n, e)| (!e).then_some(n as u8))
+            .collect(),
+    ) else {
         return;
     };
 
-    for (layer, polygon) in path.polygons() {
-        let layer = &mesh.layers[layer as usize];
-        display_polygon_gizmo(
-            layer,
-            polygon,
-            &Transform::from_rotation(Quat::from_rotation_x(FRAC_PI_2)).into(),
-            palettes::tailwind::BLUE_500.into(),
-            &mut gizmos,
-        );
-    }
+    // for (layer, polygon) in path.polygons() {
+    //     let layer = &mesh.layers[layer as usize];
+    //     display_polygon_gizmo(
+    //         layer,
+    //         polygon,
+    //         &Transform::from_rotation(Quat::from_rotation_x(FRAC_PI_2)).into(),
+    //         palettes::tailwind::BLUE_500.into(),
+    //         &mut gizmos,
+    //     );
+    // }
 
     let mut path_with_height = path.path_with_height(start, end, mesh);
 
@@ -131,4 +163,67 @@ fn draw_path(mut path_gizmos: Gizmos<PathGizmos>, mut gizmos: Gizmos, recast: Re
     }
     path_with_height.insert(0, start);
     path_gizmos.linestrip(path_with_height, palettes::tailwind::LIME_600);
+}
+
+fn layers_info(mut commands: Commands, layers: Res<Layers>, texts: Query<Entity, With<Text>>) {
+    if let Ok(entity) = texts.single() {
+        commands.entity(entity).despawn();
+    }
+    commands
+        .spawn((
+            Text::default(),
+            Node {
+                align_self: AlignSelf::FlexStart,
+                margin: UiRect::all(Val::Px(15.0)),
+                ..Default::default()
+            },
+        ))
+        .with_children(|p| {
+            let font_size = TextFont {
+                font_size: 15.0,
+                ..default()
+            };
+            for layer in layers.0.iter().enumerate() {
+                let color = LAYER_COLORS[layer.0].clone().into();
+                p.spawn((
+                    TextSpan::new(format!("Layer {}: ", layer.0)),
+                    TextColor(color),
+                    font_size.clone(),
+                ));
+                if *layer.1 {
+                    p.spawn((
+                        TextSpan::new(format!("{}\n", layer.1)),
+                        TextColor(palettes::css::GREEN.into()),
+                        font_size.clone(),
+                    ));
+                } else {
+                    p.spawn((
+                        TextSpan::new(format!("{}\n", layer.1)),
+                        TextColor(palettes::css::RED.into()),
+                        font_size.clone(),
+                    ));
+                }
+            }
+        });
+}
+
+fn switch_layers(mut layers: ResMut<Layers>, input: Res<ButtonInput<KeyCode>>) {
+    for (index, key) in [
+        KeyCode::Digit0,
+        KeyCode::Digit1,
+        KeyCode::Digit2,
+        KeyCode::Digit3,
+        KeyCode::Digit4,
+        KeyCode::Digit5,
+        KeyCode::Digit6,
+    ]
+    .iter()
+    .enumerate()
+    {
+        if input.just_pressed(*key) {
+            if let Some(layer) = layers.0.get_mut(index) {
+                *layer = !*layer;
+            }
+        }
+    }
 }
